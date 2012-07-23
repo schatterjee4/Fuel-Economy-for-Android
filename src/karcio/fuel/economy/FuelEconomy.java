200c32f720c05adebd2852eefcc94d5a069a4d5a
Karol Czarnecki <kczarnecki@hotmail.com>
2012-07-23 19:12:40 +0100

    first update

---
+++ src/karcio/fuel/economy/FuelEconomy.java
@@ -0,0 +1,67 @@
+package karcio.fuel.economy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class FuelEconomy extends Activity 
+{
+	private EditText editText1;
+	private EditText editText2;
+	
+	private TextView textView4;
+	private TextView textView6;
+	
+	private Button button1;
+	
+	
+	private double miles = 0;
+	private double liters = 0;
+	private double result = 0;
+	private double convertMilesToKm = 0;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        initParams();
+    }
+    
+    private void initParams()
+    {
+    	editText1 = (EditText)findViewById(R.id.editText1);
+    	editText2 = (EditText)findViewById(R.id.editText2);
+    	
+    	textView4 = (TextView)findViewById(R.id.textView4);
+    	textView6 = (TextView)findViewById(R.id.textView6);
+    	
+    	button1 = (Button)findViewById(R.id.button1);
+    	
+    	button1.setOnClickListener(new Button.OnClickListener() 
+    	{ 
+    		public void onClick (View v)
+			{ 
+				calculate(); 
+			}
+    	});
+    	
+    }
+    
+    private void calculate()
+    {
+    	miles = Double.parseDouble(editText1.getText().toString());
+    	liters = Double.parseDouble(editText2.getText().toString());
+    	
+    	convertMilesToKm = miles * 1.61;
+    	result = 100 * liters / convertMilesToKm;
+    	
+    	textView6.setText(Double.toString(convertMilesToKm));
+    	textView4.setText(Double.toString(result));
+    	
+    }
+        
+}
